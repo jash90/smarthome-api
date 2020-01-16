9f0a2aa1c366966436a2f8e2f4c26c700103b564
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2020-01-16 19:27:50 +0100

    cors 2

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { setupSwagger } from "./swagger";
+import cors from "cors";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
     setupSwagger(app);
+    app.use(cors());
     await app.listen(3099);
 }
 
